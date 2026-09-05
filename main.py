952d5c123ecf04728f49e9672eb32eb15c0efde5
MiaBurhan <bmia2684@gmail.com>
2026-09-05 05:40:18 +0500

    Stage 3: profile route token verification

--- main.py
+++ main.py
@@ -98,7 +98,7 @@ def login(creds: UserCredentials):
 # A route anyone can access, no auth required.
 # ==========================================================================
 @app.get("/public/info", status_code=status.HTTP_200_OK) 
-def public_info():
+def public_info(): 
     return {"message": "Welcome stranger! This info is public."}
 
 # ==========================================================================
